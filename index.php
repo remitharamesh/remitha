7972c22b9c7334ec9c17b0014af9a1fd8700166d
Remitha Ramesh <remitha1081999ramesh@gmail.com>
2020-01-12 14:50:53 +0530

    person details

--- index.php
+++ index.php
@@ -8,34 +8,54 @@
     <title>Document</title>
 </head>
 <body>
+    <form method="GET">
+
+    
     <table class="table">
         <tr><td>
             name
         </td>
         <td>
-            <input class="form-control" type="text">
+            <input class="form-control" type="text"name="getname">
         </td>
     </tr>
     <tr><td> rollno</td>
     
         <td>
-            <input class="form-control" type="text">
+            <input class="form-control" type="text"name="getroll">
         </td>
     </tr>
     <tr><td>college</td>
         <td>
-            <input class="form-control" type="text">
+            <input class="form-control" type="text"name="getcollege">
         </td>
     </tr>
     <tr><td
         >admission no</td>
-    <td><input class="form-control" type="text"></td></tr>
+    <td><input class="form-control" type="text"name="getadmno"></td></tr>
     <tr>
         <td></td>
         <td>
-            <Button class="btn btn-warning">submit</Button>
+            <Button class="btn btn-warning"type="submit"name="getsubmit">submit</Button>
         </td>
     </tr>
     </table>
+    </form>
 </body>
 </html>
+<?php
+if(isset($_GET["getsubmit"]))
+{
+    $name=$_GET["getname"];
+    $rollno=$_GET["getroll"];
+    $college=$_GET["getcollege"];
+    $admissionno=$_GET["getadmno"];
+    echo $name;
+    echo"<br>";
+    echo $rollno;
+    echo"<br>";
+    echo $college;
+    echo"<br>";
+    echo $admissionno;
+}
+?>
